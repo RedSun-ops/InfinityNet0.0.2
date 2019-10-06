b585edc3a0690efb88ecf9abbe9d5345688be1df
RedSun-ops <55594856+RedSun-ops@users.noreply.github.com>
2019-10-06 21:10:23 +0300

    Add files via upload

---
+++ SourceCode.asm
@@ -0,0 +1,1034 @@
+section .text
+use16
+org 0x7c00
+%include 'Graphics.asm' 
+start:
+section .data
+
+mov ax,3
+int 10h
+
+mov ah,0x0E
+mov al,'I'
+int 10h
+mov ah,0x0E
+mov al,'n'
+int 10h
+mov ah,0x0E
+mov al,'f'
+int 10h
+mov ah,0x0E
+mov al,'i'
+int 10h
+mov ah,0x0E
+mov al,'n'
+int 10h
+mov ah,0x0E
+mov al,'i'
+int 10h
+mov ah,0x0E
+mov al,'t'
+int 10h
+mov ah,0x0E
+mov al,'y'
+int 10h
+mov ah,0x0E
+mov al,'.'
+int 10h
+mov ah,0x0E
+mov al,'N'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'t'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'v'
+int 10h
+mov ah,0x0E
+mov al,'0'
+int 10h
+mov ah,0x0E
+mov al,'.'
+int 10h
+mov ah,0x0E
+mov al,'0'
+int 10h
+mov ah,0x0E
+mov al,'.'
+int 10h
+mov ah,0x0E
+mov al,'2'
+int 10h
+
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+
+mov ah,0x0E
+mov al,'!'
+int 10h
+mov ah,0x0E
+mov al,'P'
+int 10h
+mov ah,0x0E
+mov al,'R'
+int 10h
+mov ah,0x0E
+mov al,'E'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'A'
+int 10h
+mov ah,0x0E
+mov al,'L'
+int 10h
+mov ah,0x0E
+mov al,'P'
+int 10h
+mov ah,0x0E
+mov al,'H'
+int 10h
+mov ah,0x0E
+mov al,'A'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'T'
+int 10h
+mov ah,0x0E
+mov al,'E'
+int 10h
+mov ah,0x0E
+mov al,'S'
+int 10h
+mov ah,0x0E
+mov al,'T'
+int 10h
+mov ah,0x0E
+mov al,'I'
+int 10h
+mov ah,0x0E
+mov al,'N'
+int 10h
+mov ah,0x0E
+mov al,'G'
+int 10h
+mov ah,0x0E
+mov al,'!'
+int 10h
+
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+
+mov ah,0x0E
+mov al,'C'
+int 10h
+mov ah,0x0E
+mov al,'h'
+int 10h
+mov ah,0x0E
+mov al,'a'
+int 10h
+mov ah,0x0E
+mov al,'n'
+int 10h
+mov ah,0x0E
+mov al,'g'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'l'
+int 10h
+mov ah,0x0E
+mov al,'o'
+int 10h
+mov ah,0x0E
+mov al,'g'
+int 10h
+
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+
+mov ah,0x0E
+mov al,'v'
+int 10h
+mov ah,0x0E
+mov al,'0'
+int 10h
+mov ah,0x0E
+mov al,'.'
+int 10h
+mov ah,0x0E
+mov al,'0'
+int 10h
+mov ah,0x0E
+mov al,'.'
+int 10h
+mov ah,0x0E
+mov al,'1'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'-'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'D'
+int 10h
+mov ah,0x0E
+mov al,'i'
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,'p'
+int 10h
+mov ah,0x0E
+mov al,'l'
+int 10h
+mov ah,0x0E
+mov al,'a'
+int 10h
+mov ah,0x0E
+mov al,'y'
+int 10h
+mov ah,0x0E
+mov al,'i'
+int 10h
+mov ah,0x0E
+mov al,'n'
+int 10h
+mov ah,0x0E
+mov al,'g'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'T'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'x'
+int 10h
+mov ah,0x0E
+mov al,'t'
+int 10h
+
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+
+mov ah,0x0E
+mov al,'v'
+int 10h
+mov ah,0x0E
+mov al,'0'
+int 10h
+mov ah,0x0E
+mov al,'.'
+int 10h
+mov ah,0x0E
+mov al,'0'
+int 10h
+mov ah,0x0E
+mov al,'.'
+int 10h
+mov ah,0x0E
+mov al,'2'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'-'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'M'
+int 10h
+mov ah,0x0E
+mov al,'o'
+int 10h
+mov ah,0x0E
+mov al,'r'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'T'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'x'
+int 10h
+mov ah,0x0E
+mov al,'t'
+int 10h
+
+jmp $
